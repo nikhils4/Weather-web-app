b57060841485cc3bf60d42e9b9a908d8c74be714
Craig Ramsden <Crammers@users.noreply.github.com>
2019-10-06 18:30:06 +0100

    Update reverse.js
    Added config

--- reverse.js
+++ reverse.js
@@ -1,9 +1,9 @@
 const request = require("request");
-
+const config = require('./config');
 
 var reverse = (lat, lan, callback) => {
     request ({
-        url : 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + lan + '&pretty=1&key=5db5a96f174d46e389853c0ec12d44a2',
+        url : 'https://api.opencagedata.com/geocode/v1/json?q=' + lat + '+' + lan + '&pretty=1&key=' + config.opencage + ',
         json : true
     }, (error, response, body) => {
         if(error){
